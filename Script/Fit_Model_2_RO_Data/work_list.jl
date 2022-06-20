3413e5b7644124b8c263f64f94c84da84e0bf3e5
PeterFransson <peter.fransson@gmail.com>
2022-06-20 13:12:40 +0200

    work_list update

--- Script/Fit_Model_2_RO_Data/work_list.jl
+++ Script/Fit_Model_2_RO_Data/work_list.jl
@@ -544,22 +544,22 @@ function work_list()
         
     calibparavec = CalibParaVec(
         (:Nₛ,0.0001,0.1,true),(:a_Jmax,0.01,1.0),
-        (:Kₓₗ₀,0.0005,0.1,true),(:i,0.1,10.0),
+        (:Kₓₗ₀,0.0005,0.1,true),
         (:α_max,0.1,0.5),
         (:τ,1.0,15.0),(:Smax,10.0,25.0),                        
         (:a_GPP,0.0001,5.0),(:b_GPP,0.0001,3.0))#,(:a_Ec,0.0001,5.0),(:b_Ec,0.0001,3.0)) 
-    ParaDictInit_C = Dict(:μ_Nₘ_f=>0.0113,:b_Nₘ_f=>0.0018)   
-    ParaDictInit_F = Dict(:μ_Nₘ_f=>0.018,:b_Nₘ_f=>0.0018) 
+    ParaDictInit_C = Dict(:μ_Nₘ_f=>0.0113,:b_Nₘ_f=>0.0018,:a_Ec=>0.0433,:b_Ec=>0.179)   
+    ParaDictInit_F = Dict(:μ_Nₘ_f=>0.018,:b_Nₘ_f=>0.0018,:a_Ec=>0.0688,:b_Ec=>0.146) 
     ranges,para2ind = CreateOptVar(calibparavec) 
-    file_name = "RO_Opt_GPP_Nm_f_20220608"
+    file_name = "RO_Opt_GPP_Ec_Nm_f_20220619_2"
     
     run_CrossValidation(file_name,
     10,
     ranges,
     para2ind;
-    PopulationSize=20,
-    MaxSteps=300,
-    Calc_logP=Calc_logP_GPP_Nm_f,
+    PopulationSize=300,
+    MaxSteps=30000,
+    Calc_logP=Calc_logP_GPP_Ec_Nm_f,
     ParaDictInit_C=ParaDictInit_C,
     ParaDictInit_F=ParaDictInit_F)    
 
